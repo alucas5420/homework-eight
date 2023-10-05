5f3184f31e01bbd0d30fd8e58e52410cf86f350d
alucas5420 <112018521+alucas5420@users.noreply.github.com>
2023-10-05 11:04:44 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,318 @@
+var homeContent = `<header>
+<a>NINTENDO</a>
+</header>
+<section class="hero"></section>
+<header2>
+<a>Welcome to the official Nintendo site!</a>
+</header2>
+<section class="aboutContent">
+<br>
+<h1>Welcome to the Nintendo home page!</h1>
+<br>
+<h1>Explore the many different products that we sell</h1>
+<br>
+<h1>Click any of the boxes below to explore the different options available! </h1>
+
+<div class="boxes">
+    <div class="box"> 
+        <br>
+        <br> 
+        <p>Nintendo Switch</p>
+        </div>
+        <div class="box">
+            <br>
+            <br>
+        <p>Games for Switch</p>
+        </div>
+        <div class="box">
+            <br>
+            <br>
+            <p>3DS</p> 
+        </div>
+        <div class="box">
+            <br>
+            <br>
+            <p>Games for 3DS</p> 
+        </div>
+</div>
+
+</section>
+<header2></header2>
+<section class="hero4"></section>
+<header2></header2>
+<section class="aboutContent">
+<br>
+
+<h1>My Nintendo Store</h1>
+<br>
+<h1>Check off your list with our Holiday Gift Guide!</h1>
+<br>
+<div class="boxes">
+    <div class="box"> 
+        <br>
+        <br> 
+        <p>My Nintendo Store</p>
+        </div>
+        </div>
+</section>
+
+<header2></header2>
+<section class="hero2"></section>
+<header2></header2>
+<section class="aboutContent">
+<br>
+<h1>News</h1>
+<br>
+<h1>Be sure to check out the news below!</h1>
+<br>
+<div class="boxes">
+  <div class="box"> 
+      <br>
+      <br> 
+      <p>News</p>
+      </div>
+      </div>
+</section>
+<header>
+<a>NINTENDO</a>
+</header>`;
+
+var aboutContent = ` <header>
+<a>NINTENDO</a>
+</header>
+<section class="hero5"></section>
+<header2>
+<a>Welcome to the official Nintendo site!</a>
+</header2>
+<section class="aboutContent">
+<br>
+<h1>Welcome to the Nintendo about page!</h1>
+<br>
+<h1>Learn about the many different products that we sell</h1>
+<br>
+<h1>Click any of the boxes below to learn about the different options available! </h1>
+
+<div class="boxes">
+    <div class="box"> 
+        <br>
+        <br> 
+        <p>Nintendo Switch</p>
+        </div>
+        <div class="box">
+            <br>
+            <br>
+        <p>Games for Switch</p>
+        </div>
+        <div class="box">
+            <br>
+            <br>
+            <p>3DS</p> 
+        </div>
+        <div class="box">
+            <br>
+            <br>
+            <p>Games for 3DS</p> 
+        </div>
+</div>
+
+</section>
+<header2></header2>
+<section class="hero4"></section>
+<header2></header2>
+<section class="aboutContent">
+<br>
+
+<h1>Nintendo Switch</h1>
+<br>
+<h1>Learn more in-depth about the Nintendo Switch!</h1>
+<br>
+<div class="boxes">
+    <div class="box"> 
+        <br>
+        <br> 
+        <p>Nintendo Switch</p>
+        </div>
+        </div>
+</section>
+
+<header2></header2>
+<section class="hero3"></section>
+<header2></header2>
+<section class="aboutContent">
+<br>
+<h1>3DS</h1>
+<br>
+<h1>Learn more in-depth about the 3DS!</h1>
+<br>
+<div class="boxes">
+  <div class="box"> 
+      <br>
+      <br> 
+      <p>3DS</p>
+      </div>
+      </div>
+</section>
+<header>
+<a>NINTENDO</a>
+</header>`;
+var productsContent = `<header>
+<a>NINTENDO</a>
+</header>
+<section class="hero"></section>
+<header2>
+<a>Welcome to the official Nintendo site!</a>
+</header2>
+<section class="aboutContent">
+<br>
+<h1>Welcome to the Nintendo products</h1>
+<br>
+<h1>Explore the most popular series</h1>
+<br>
+<h1>Click any of the boxes below to explore the different major series! </h1>
+
+<div class="boxes">
+    <div class="box"> 
+        <br>
+        <br> 
+        <p>Super Mario</p>
+        </div>
+        <div class="box">
+            <br>
+            <br>
+        <p>The Legend of Zelda</p>
+        </div>
+        <div class="box">
+            <br>
+            <br>
+            <p>Pokemon</p> 
+        </div>
+        <div class="box">
+            <br>
+            <br>
+            <p>Splatoon</p> 
+        </div>
+</div>
+
+</section>
+<header2></header2>
+<section class="hero6"></section>
+<header2></header2>
+<section class="aboutContent">
+<br>
+
+<h1>Super Mario</h1>
+<br>
+<h1>Click on the button below to learn more about Super Mario!</h1>
+<br>
+<div class="boxes">
+    <div class="box"> 
+        <br>
+        <br> 
+        <p>Super Mario</p>
+        </div>
+        </div>
+</section>
+
+<header2></header2>
+<section class="hero7"></section>
+<header2></header2>
+<section class="aboutContent">
+<br>
+<h1>The Legend of Zelda</h1>
+<br>
+<h1>Click on the button below to learn more about The Legend of Zelda!</h1>
+<br>
+<div class="boxes">
+  <div class="box"> 
+      <br>
+      <br> 
+      <p>The Legend of Zelda</p>
+      </div>
+      </div>
+</section>
+<header>
+<a>NINTENDO</a>
+</header>`;
+var contactContent = `<header>
+<a>NINTENDO</a>
+</header>
+<section class="hero5"></section>
+<header2>
+<a>Welcome to the official Nintendo site!</a>
+</header2>
+<section class="aboutContent">
+<br>
+<h1>Welcome to the Nintendo contact page!</h1>
+<br>
+<h1>Figure out the many different ways to contact us</h1>
+<br>
+<h1>Click any of the boxes below to explore the different contact options available! </h1>
+
+<div class="boxes">
+    <div class="box"> 
+        <br>
+        <br> 
+        <p>Email</p>
+        </div>
+        <div class="box">
+            <br>
+            <br>
+        <p>Phone</p>
+        </div>
+</div>
+
+</section>
+<header2></header2>
+<section class="hero8"></section>
+<header2></header2>
+<section class="aboutContent">
+<br>
+
+<h1>Email</h1>
+<br>
+<h1>Click the button below to email us!</h1>
+<br>
+<div class="boxes">
+    <div class="box"> 
+        <br>
+        <br> 
+        <p>Email</p>
+        </div>
+        </div>
+</section>
+
+<header2></header2>
+<section class="hero9"></section>
+<header2></header2>
+<section class="aboutContent">
+<br>
+<h1>Phone</h1>
+<br>
+<h1>Click the button below to call us!</h1>
+<br>
+<div class="boxes">
+  <div class="box"> 
+      <br>
+      <br> 
+      <p>Phone</p>
+      </div>
+      </div>
+</section>
+<header>
+<a>NINTENDO</a>
+</header>`;
+
+function initListeners(){
+    $("nav a").click(function(e){
+        let aID = e.currentTarget.id;
+        let contentID = aID + "Content";
+        
+        $("#app").html(eval(contentID))
+
+    });
+}
+
+$(document).ready(function (){ 
+    initListeners();
+
+});
